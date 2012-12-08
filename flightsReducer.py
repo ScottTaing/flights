bb7344f7f01842fb34a397eb1c00f5cf9b76d3b7
asood <atinsood@gmail.com>
2012-12-08 16:09:03 -0500

    fixing bug in counter

--- flightsReducer.py
+++ flightsReducer.py
@@ -4,20 +4,20 @@ import sys
 import json
 
 (last_key, total) = (None, 0)
-counter = 1
+counter = 0
 for line in sys.stdin:
    (tempKey, tempVal) = line.strip().split('\t')
    (key, val) = (tempKey, json.loads(tempVal))
    if last_key and last_key != key:
         #Means we are seeing a new key
+        counter = counter + 1
         print '%s\t%s' %(last_key, (total,counter))
         (last_key, total) = (key, (float(val[4])))
-        counter = counter + 1
    else:
         #We enter here for the very first time when the key is NONE
         #or when the key is not changing
-        (last_key, total) = (key,((float(val[4])) + total))
         counter = counter + 1
+        (last_key, total) = (key,((float(val[4])) + total))
 
 if last_key:
     #This takes care of the last key if last key is unique
